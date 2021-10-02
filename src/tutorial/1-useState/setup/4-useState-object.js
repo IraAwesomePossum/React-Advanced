387c33ba540174d36ba76e9e6d7c0236dc778f14
Irina <ifalko@protonmail.com>
2021-10-02 18:08:46 +0200

    Regular counter using useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Irina",
+    age: 30,
+    message: "You are the best",
+  });
+
+  const [name, setName] = useState("Philip");
+  const [age, setAge] = useState(32);
+  const [message, setMessage] = useState("the best muzh");
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: "you are a star" });
+    setMessage("you are a star");
+  };
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
